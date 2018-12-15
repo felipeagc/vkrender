bdb3ce22c90ad9070f8c40daa23b8a952018180d
Felipe <felipeagc2@gmail.com>
2018-12-15 19:26:03 -0200

    renderer: move setImageLayout and use it in buffer to image copies

--- renderer/renderer/buffer.cpp
+++ renderer/renderer/buffer.cpp
@@ -152,64 +152,12 @@ void Buffer::bufferTransfer(Buffer &to, size_t size) {
 void Buffer::imageTransfer(VkImage toImage, uint32_t width, uint32_t height) {
   VkCommandBuffer commandBuffer = beginSingleTimeCommandBuffer();
 
-  auto transitionImageLayout = [&](VkImageLayout oldLayout,
-                                   VkImageLayout newLayout) {
-    VkImageMemoryBarrier barrier{
-        VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
-        nullptr,
-        {},                      // srcAccessMask (we'll set this later)
-        {},                      // dstAccessMask (we'll set this later)
-        oldLayout,               // oldLayout
-        newLayout,               // newLayout
-        VK_QUEUE_FAMILY_IGNORED, // srcQueueFamilyIndex
-        VK_QUEUE_FAMILY_IGNORED, // dstQueueFamilyIndex
-        toImage,                 // image
-        {
-            VK_IMAGE_ASPECT_COLOR_BIT, // aspectMask
-            0,                         // baseMipLevel
-            1,                         // levelCount
-            0,                         // baseArrayLayer
-            1,                         // layerCount
-        },                             // subresourceRange
-    };
-
-    VkPipelineStageFlags srcStage;
-    VkPipelineStageFlags dstStage;
-
-    if (oldLayout == VK_IMAGE_LAYOUT_UNDEFINED &&
-        newLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL) {
-      barrier.srcAccessMask = 0;
-      barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
-
-      srcStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
-      dstStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
-    } else if (
-        oldLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL &&
-        newLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) {
-      barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
-      barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
-
-      srcStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
-      dstStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
-    } else {
-      throw std::runtime_error("Unsupported layout transition!");
-    }
-
-    vkCmdPipelineBarrier(
-        commandBuffer,
-        srcStage,
-        dstStage,
-        0,
-        0,
-        nullptr,
-        0,
-        nullptr,
-        1,
-        &barrier);
-  };
-
-  transitionImageLayout(
-      VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+  setImageLayout(
+      commandBuffer,
+      toImage,
+      VK_IMAGE_ASPECT_COLOR_BIT,
+      VK_IMAGE_LAYOUT_UNDEFINED,
+      VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
 
   VkBufferImageCopy region{
       0, // bufferOffset
@@ -233,7 +181,10 @@ void Buffer::imageTransfer(VkImage toImage, uint32_t width, uint32_t height) {
       1,
       &region);
 
-  transitionImageLayout(
+  setImageLayout(
+      commandBuffer,
+      toImage,
+      VK_IMAGE_ASPECT_COLOR_BIT,
       VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
       VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
 
--- renderer/renderer/util.hpp
+++ renderer/renderer/util.hpp
@@ -10,3 +10,23 @@
   } while (0)
 
 #define ARRAYSIZE(array) (sizeof(array) / sizeof((array)[0]))
+
+namespace renderer {
+void setImageLayout(
+    VkCommandBuffer cmdbuffer,
+    VkImage image,
+    VkImageLayout oldImageLayout,
+    VkImageLayout newImageLayout,
+    VkImageSubresourceRange subresourceRange,
+    VkPipelineStageFlags srcStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
+    VkPipelineStageFlags dstStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT);
+
+void setImageLayout(
+    VkCommandBuffer cmdbuffer,
+    VkImage image,
+    VkImageAspectFlags aspectMask,
+    VkImageLayout oldImageLayout,
+    VkImageLayout newImageLayout,
+    VkPipelineStageFlags srcStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
+    VkPipelineStageFlags dstStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT);
+} // namespace renderer
