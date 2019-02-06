19ee83f1e7eab687ec6e5364adb5f3d5c9e51400
Felipe <felipeagc2@gmail.com>
2019-02-06 19:32:22 -0200

    vmath: fix vec3_lerp function

--- vmath/vmath/misc.h
+++ vmath/vmath/misc.h
@@ -9,7 +9,7 @@ extern "C" {
 #include "vec4.h"
 
 inline vec3_t vec3_lerp(vec3_t v1, vec3_t v2, float t) {
-  return vec3_mul(vec3_adds(v1, t), vec3_sub(v2, v1));
+  return vec3_add(v1, vec3_muls(vec3_sub(v2, v1), t));
 }
 
 inline float to_radians(float degrees) { return degrees * (M_PI / 180.0f); }
