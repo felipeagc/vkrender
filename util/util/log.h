84d897ec94fcce045cecfa54e6b8beb1973fb5a1
Felipe <felipeagc2@gmail.com>
2019-02-23 04:08:28 -0300

    util: temporarily change logging to avoid buffer overflows

--- util/util/log.h
+++ util/util/log.h
@@ -8,40 +8,39 @@ extern "C" {
 #include <stdio.h>
 #include <string.h>
 
-inline void ut_vprintf(const char *prefix, const char *format, va_list args) {
-  char buf[512];
-  strcpy(buf, "");
-  strcat(buf, prefix);
-  strcat(buf, format);
-  strcat(buf, "\n");
-  vprintf(buf, args);
-}
-
 inline void ut_log_info(const char *format, ...) {
   va_list args;
   va_start(args, format);
-  ut_vprintf("[INFO] ", format, args);
+  printf("[INFO] ");
+  vprintf(format, args);
+  printf("\n");
   va_end(args);
 }
 
 inline void ut_log_fatal(const char *format, ...) {
   va_list args;
   va_start(args, format);
-  ut_vprintf("[FATAL] ", format, args);
+  printf("[FATAL] ");
+  vprintf(format, args);
+  printf("\n");
   va_end(args);
 }
 
 inline void ut_log_error(const char *format, ...) {
   va_list args;
   va_start(args, format);
-  ut_vprintf("[ERROR] ", format, args);
+  printf("[ERROR] ");
+  vprintf(format, args);
+  printf("\n");
   va_end(args);
 }
 
 inline void ut_log_warn(const char *format, ...) {
   va_list args;
   va_start(args, format);
-  ut_vprintf("[WARN] ", format, args);
+  printf("[WARN] ");
+  vprintf(format, args);
+  printf("\n");
   va_end(args);
 }
 
@@ -49,7 +48,9 @@ inline void ut_log_debug(const char *format, ...) {
 #ifndef NDEBUG
   va_list args;
   va_start(args, format);
-  ut_vprintf("[DEBUG] ", format, args);
+  printf("[DEBUG] ");
+  vprintf(format, args);
+  printf("\n");
   va_end(args);
 #endif
 }
