6be29739f9492170a4e99f91028df2b4e940fb75
Felipe <felipeagc2@gmail.com>
2019-03-09 03:24:21 -0300

    Fix transform_component transformation order

--- engine/engine/components/transform_component.c
+++ engine/engine/components/transform_component.c
@@ -8,10 +8,11 @@ void eg_transform_component_init(eg_transform_component_t *transform) {
 }
 
 mat4_t eg_transform_component_to_mat4(eg_transform_component_t *transform) {
-  mat4_t mat = mat4_scale(mat4_identity(), transform->scale);
-  mat = mat4_rotate(mat, transform->angle, transform->axis);
-  mat = mat4_translate(mat, transform->position);
-  return mat;
+  mat4_t scale = mat4_scale(mat4_identity(), transform->scale);
+  mat4_t rotation = mat4_rotate(mat4_identity(), transform->angle, transform->axis);
+  mat4_t translation = mat4_translate(mat4_identity(), transform->position);
+
+  return mat4_mul(mat4_mul(scale, rotation), translation);
 }
 
 void eg_transform_component_destroy(eg_transform_component_t *transform) {}
